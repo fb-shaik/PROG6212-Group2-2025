0a134d3d500480e71ee0f48a0197598e2bd1b5c5
fb-shaik <128546053+fb-shaik@users.noreply.github.com>
2025-10-22 18:50:32 +0200

    Attributes_Demo_WPF

---
+++ Attributes_Demo_WPF/Attributes_Demo_WPF/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Attributes_Demo_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            var student = new Student();
+            {
+                student.Name = NameBox.Text.Trim();
+                student.Email = EmailBox.Text.Trim();
+                student.Age = int.TryParse(AgeBox.Text , out var age) ? age : 0;
+            }
+            var results = new List<System.ComponentModel.DataAnnotations.ValidationResult>();
+            bool isValid = Validator.TryValidateObject(student, new ValidationContext(student), results, true);
+
+
+            if (isValid)
+            {
+                ResultBlock.Text = "✅ Registration successful!";
+                ResultBlock.Foreground = Brushes.Green;
+            }
+            else
+            {
+                var errorMessages = results.ConvertAll(r => "- " + r.ErrorMessage);
+                ResultBlock.Text = "❌ Errors:\n" + string.Join("\n", errorMessages);
+                ResultBlock.Foreground = Brushes.Red;
+            }
+
+        }
+    }
+}
---
+++ Attributes_Demo_WPF/Attributes_Demo_WPF/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes_Demo_WPF
+{
+    public class Student
+    {
+        [Required(ErrorMessage ="Name is required.")]
+        [StringLength(50, ErrorMessage ="Name must be at most 50 characters")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress(ErrorMessage ="Invalid email")]
+        public string Email { get; set; }
+
+        [Range(16,35, ErrorMessage ="Age must be between 16 & 35")]
+        public int Age { get; set; }
+    }
+}
